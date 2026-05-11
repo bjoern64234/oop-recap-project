3d12ae78843178608cc2fd36eccc9a9b50754f1a
bjoern <b.irion@wtnet.de>
2026-05-11 10:08:58 +0200

    Create shopService with methods

---
+++ src/main/java/ShopService.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+public class ShopService {
+
+    private final ProductRepo productRepo;
+    private final OrderListRepo orderListRepo;
+
+    public ShopService(ProductRepo productRepo, OrderListRepo orderListRepo) {
+        this.productRepo = productRepo;
+        this.orderListRepo = orderListRepo;
+    }
+
+    public void addItemToStock(String name, double price) {
+        Product product = new Product(UUID.randomUUID().toString(), name, price);
+        this.productRepo.add(product);
+    }
+
+    public List<Product> sales() {
+        return this.productRepo.getProducts();
+    }
+
+    public void order(String productId, int amount) {
+        Product product = this.productRepo.getById(productId);
+
+        if (product == null) {
+            System.out.println("A product with the productId " + productId + " is not existing");
+            return;
+        }
+
+        Order order = new Order(UUID.randomUUID().toString(), productId, amount);
+        this.orderListRepo.add(order);
+    }
+
+    public void remove(String productId) {
+        Order order = this.getOrderByProductId(productId);
+
+        this.orderListRepo.remove(order);
+    }
+
+    public double getPriceOfOrderByProductId(String productId) {
+        Order order = this.getOrderByProductId(productId);
+        Product product = this.productRepo.getById(productId);
+
+        return order.amount() * product.price();
+    }
+
+    public double getTotal() {
+        List<Order> orders = this.getAllOrders();
+        double total = 0.0;
+
+        for (Order order : orders) {
+            total += this.getPriceOfOrderByProductId(order.productId());
+        }
+
+        return total;
+    }
+
+    public List<Order> getAllOrders() {
+        return new ArrayList<>(this.orderListRepo.getAll());
+    }
+
+    public Order getOrderByProductId(String productId) {
+        List<Order> orders = this.orderListRepo.getAll();
+
+        for (Order order : orders) {
+            if (productId.equals(order.productId())) {
+                return order;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ShopService that = (ShopService) o;
+        return Objects.equals(productRepo, that.productRepo) && Objects.equals(orderListRepo, that.orderListRepo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productRepo, orderListRepo);
+    }
+
+    @Override
+    public String toString() {
+        return "ShopService{" +
+                "productRepo=" + productRepo +
+                ", orderListRepo=" + orderListRepo +
+                '}';
+    }
+}
