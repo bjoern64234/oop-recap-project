3d3af66782084c19cb74b9fed50a7c1ca5ac6bab
bjoern <b.irion@wtnet.de>
2026-05-19 09:22:24 +0200

    Use lombok for order

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,15 +1,5 @@
+import lombok.With;
+
+@With
 public record Order(String uuid, String productId, int amount, OrderStatus status) {
-
-    public Order withAmount(int newAmount) {
-        if (newAmount < 1) {
-            System.out.println("You cannot order less than 1");
-            return this;
-        }
-
-        return new Order(this.uuid, this.productId, newAmount, this.status);
-    }
-
-    public Order withStatus(OrderStatus status) {
-        return new Order(this.uuid, this.productId, this.amount, status);
-    }
 }
